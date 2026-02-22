efcf24ee8ea32e8c11c62c0ea81773d74684cb8b
AritraApon <aritromazumdar8@gmail.com>
2026-02-22 22:02:03 +0600

    side 0 of 0 count add funtion

--- node.js
+++ node.js
@@ -15,6 +15,8 @@ const allSection = document.getElementById('allSection');
 // interview array ane rejected array
 const totalAvNum = document.getElementById('totalAvNum');
 const AvailableJobNumber = document.getElementById('AvailableJobNumber');
+const interviewAvNUM = document.getElementById('interviewAvNUM');
+const rejectedAvNUM = document.getElementById('rejectedAvNUM');
 
 
 
@@ -24,37 +26,39 @@ let rejectedList = [];
 
 function cardNumberCount() {
     totalAvNum.innerText = allSection.children.length;
+    interviewAvNUM.innerText =interviewList.length;
+    rejectedAvNUM.innerText =rejectedList.length;
     total.innerText = allSection.children.length;
     interview.innerText = interviewList.length;
     rejected.innerText = rejectedList.length;
 
-
 }
 cardNumberCount()
+
+
 
 
 // toggle button
 const toggleAllBtn = document.getElementById('toggleAllBtn');
 const toggleInterviewBtn = document.getElementById('toggleInterviewBtn');
 const toggleRejectedBtn = document.getElementById('toggleRejectedBtn');
+const oF = document.getElementById('oF');
 
-
-const interviewAvNUM = document.getElementById('interviewAvNUM');
-const rejectedAvNUM = document.getElementById('rejectedAvNUM');
-
-function toggleClick(id1, id2, id3) {
+function toggleClick(id1, id2, id3 ,id4) {
     //    bg add
     toggleAllBtn.classList.add("text-base-500", "bg-base-300");
     toggleInterviewBtn.classList.add("text-base-500", "bg-base-300");
     toggleRejectedBtn.classList.add("text-base-500", "bg-base-300");
     // -----------------------------------------------------------------
-    allSection.classList.add('hidden')
-    interviewCards.classList.add('hidden')
-    rejectedCards.classList.add('hidden')
+    allSection.classList.add('hidden');
+    interviewCards.classList.add('hidden');
+    rejectedCards.classList.add('hidden');
     // -----------------------------------------------------------------
 
-    interviewAvNUM.classList.add('hidden')
-    rejectedAvNUM.classList.add('hidden')
+    interviewAvNUM.classList.add('hidden');
+    rejectedAvNUM.classList.add('hidden');
+    oF.classList.add('hidden');
+
 
 
     // bg- remove
@@ -76,18 +80,12 @@ function toggleClick(id1, id2, id3) {
     showNUM.classList.remove("hidden");
 
 
+    const showOF = document.getElementById(id4);
+    showOF.classList.remove("hidden");
+
 }
 
-// Delete Button
-//   const dltBtn = document.querySelectorAll(".dltBtn");
-//   for(let btn of dltBtn) {
-//     btn.addEventListener("click", function(event) {
-//       event.target.parentNode.parentElement.remove();
 
-//       cardNumberCount()
-//     });
-
-//   };
 const main = document.getElementById("main_div");
 
 main.addEventListener("click", function (event) {
@@ -95,7 +93,9 @@ main.addEventListener("click", function (event) {
         event.target.parentNode.parentElement.remove();
         cardNumberCount();
     }
+    cardNumberCount()
 });
+
 
 
 
